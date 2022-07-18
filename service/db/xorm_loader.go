61b3c96563cb5fb478e0f02d9db72b79b63d6945
zhanghup <zhang3611@qq.com>
2022-07-18 14:58:38 +0800

    +++

--- service/db/xorm_loader.go
+++ service/db/xorm_loader.go
@@ -6,23 +6,23 @@ import (
 	"github.com/zhanghup/go-tools.v2/service/dm"
 )
 
-// Slice 查找数据库对象,ctx可以为nil
-func Slice[Result any](ctx context.Context, beanKey, beanNameOrSql string, field string, param ...any) ([]Result, error) {
+// Slice2 查找数据库对象,ctx可以为nil
+func Slice2[Result any](ctx context.Context, beanKey, beanNameOrSql string, field string, param ...any) ([]Result, error) {
 	return dm.Slice[Result](engine, ctx, beanKey, beanNameOrSql, field, param...)
 }
 
-func Slice2[Result any](ctx context.Context, beanKey, field string, param ...any) ([]Result, error) {
+func Slice[Result any](ctx context.Context, beanKey, field string, param ...any) ([]Result, error) {
 	tab := tools.RftTypeInfo(new(Result))
 	tableName := Default().GetTableMapper().Obj2Table(tab.Name)
 	return dm.Slice[Result](engine, ctx, beanKey, tableName, field, param...)
 }
 
-// Info 根据id查找数据库对象,ctx可以为nil
-func Info[Result any](ctx context.Context, beanKey, beanNameOrSql string, field string, param ...any) (*Result, error) {
+// Info2 根据id查找数据库对象,ctx可以为nil
+func Info2[Result any](ctx context.Context, beanKey, beanNameOrSql string, field string, param ...any) (*Result, error) {
 	return dm.Info[Result](engine, ctx, beanKey, beanNameOrSql, field, param...)
 }
 
-func Info2[Result any](ctx context.Context, beanKey, field string, param ...any) (*Result, error) {
+func Info[Result any](ctx context.Context, beanKey, field string, param ...any) (*Result, error) {
 	tab := tools.RftTypeInfo(new(Result))
 	tableName := Default().GetTableMapper().Obj2Table(tab.Name)
 
