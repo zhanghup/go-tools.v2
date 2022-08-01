08feba138800841b399d769d938b5d2f1c2f1f79
zhanghup <zhang3611@qq.com>
2022-08-01 10:47:46 +0800

    ++

--- service/db/tests/xorm_init.go
+++ service/db/tests/xorm_init.go
@@ -3,7 +3,6 @@ package tests
 import (
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/zhanghup/go-tools.v2"
 	"github.com/zhanghup/go-tools.v2/service/db"
 )
 
@@ -15,44 +14,86 @@ type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
+type UserDep struct {
+	Id  string `json:"id"`
+	Uid string `json:"uid"`
+	Dep string `json:"dep"`
+}
+
+type Dep struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
 
 func init() {
 	err := db.Init(db.Config{
 		Uri:    "./data.db",
 		Driver: "sqlite3",
-		Debug:  false,
+		Debug:  true,
 	})
 	if err != nil {
 		panic(err)
 	}
-	db.Default().DropTables(User{})
-	db.Default().Sync2(User{})
+	db.Default().DropTables(User{}, UserDep{}, Dep{})
+	db.Default().Sync2(User{}, UserDep{}, Dep{})
 
 	err = db.Insert(nil, User{
-		Id:   "111",
-		Name: "zander",
-		Age:  999,
+		Id:   "1",
+		Name: "张三",
+		Age:  18,
 
 		Username: fmt.Sprintf("%d", 888),
 		Password: "Aa123456.",
-	})
+	}, User{
+		Id:   "2",
+		Name: "李四",
+		Age:  19,
 
+		Username: fmt.Sprintf("%d", 888),
+		Password: "Aa123456.",
+	}, User{
+		Id:       "3",
+		Name:     "王五",
+		Age:      20,
+		Username: fmt.Sprintf("%d", 888),
+		Password: "Aa123456.",
+	})
 	if err != nil {
 		panic(err)
 	}
 
-	for i := 0; i < 9; i++ {
-		err := db.Session[User](nil).Insert(User{
-			Id:   tools.UUID(),
-			Name: "zander",
-			Age:  i,
+	err = db.Insert(nil, Dep{
+		Id:   "1",
+		Name: "A部门",
+	}, Dep{
+		Id:   "2",
+		Name: "B部门",
+	}, Dep{
+		Id:   "3",
+		Name: "C部门",
+	})
+	if err != nil {
+		panic(err)
+	}
 
-			Username: fmt.Sprintf("%d", i),
-			Password: "Aa123456.",
-		})
-
-		if err != nil {
-			panic(err)
-		}
+	err = db.Insert(nil, UserDep{
+		Id:  "1",
+		Uid: "1",
+		Dep: "1",
+	}, UserDep{
+		Id:  "2",
+		Uid: "1",
+		Dep: "2",
+	}, UserDep{
+		Id:  "3",
+		Uid: "2",
+		Dep: "1",
+	}, UserDep{
+		Id:  "4",
+		Uid: "3",
+		Dep: "3",
+	})
+	if err != nil {
+		panic(err)
 	}
 }
